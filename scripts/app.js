51b8edcddf9ab1e086c203674b63c0396b09367e
PATRICK-PC\bchen <bchen09@syr.edu>
2017-04-13 13:57:55 -0600

    checkpoint-4-completed

--- scripts/app.js
+++ scripts/app.js
@@ -1,20 +1,27 @@
 (function () {
     function config($locationProvider) {
         $locationProvider
-            .html5Mode({
+            .html5Mode({ //hashBang URLs are disabled
                 enabled: true,
                 requireBase: false
             });
 
 
     };
+
+
+
+
+
     angular
         .module('blocTime', ['firebase', 'ui.router'])
         .config(config)
         .constant('STOP_WATCH', {
-            "workTime": 5,
-            "breakTime": 3,
+            "totalWorkTime": 5,
+            "totalBreakTime": 3,
+            "totalLongBreakTime": 4,
             "defaultWorkTime": 5,
-            "defaultBreakTime": 3
+            "defaultBreakTime": 3,
+            "defaultLongBreakTime": 4
         });
 })();
--- scripts/directives/clocktimer.js
+++ scripts/directives/clocktimer.js
@@ -1,4 +1,4 @@
-(function () {
+function () {
     function clockTimer($interval, $window, STOP_WATCH) {
         return {
             templateUrl: '/templates/directives/clock_timer.html',
@@ -7,73 +7,60 @@
             scope: {},
             link: function (scope, element, attributes) {
 
-                scope.STOP_WATCH = STOP_WATCH; //see constants in app.js
+                scope.STOP_WATCH = STOP_WATCH;
                 scope.startButton = 'Start Work';
                 scope.breakButton = 'Take Break';
-                //boolean for alternating displaying of work-time or break
-                scope.onBreak = false;
 
-                // @desc Holds state for $interval call
+                scope.onBreak = false;
+                var completedWorkSessions = 0;
+
+
                 var promise;
 
-                /**
-                 * @desc counts down the working time clock
-                 * @function
-                 */
+
+
                 var workCountdown = function () {
-                    if (scope.STOP_WATCH.workTime > 0) {
-                        scope.STOP_WATCH.workTime--;
-                    } else if (scope.STOP_WATCH.workTime === 0) {
+                    if (scope.STOP_WATCH.totalWorkTime > 0) {
+                        scope.STOP_WATCH.totalWorkTime--;
+                    } else if (scope.STOP_WATCH.totalWorkTime === 0) {
                         scope.onBreak = true;
                         scope.stop();
                         scope.startButton = 'Start Work';
-                        scope.STOP_WATCH.workTime = scope.STOP_WATCH.defaultWorkTime;
+                        scope.STOP_WATCH.totalWorkTime = scope.STOP_WATCH.defaultWorkTime;
+                        completedWorkSessions++;
+                        if (completedWorkSessions % 4 === 0) {
+                            scope.STOP_WATCH.totalBreakTime = scope.STOP_WATCH.defaultLongBreakTime;
+                            scope.breakButton = 'Take Long Break';
+                        }
+                        console.log(completedWorkSessions);
                     }
                 };
 
-                // function countDown(time) {
-                //   if (time > 0) {
-                //     console.log(time);
-                //     time--;
-                //   } else if (time === 0) {
-                //     scope.onBreak = (onBreak === true) ? false : true;
-                //     defaultAll();
-                //   }
-                // };
-                //
-                // var defaultAll = function() {
-                //   scope.stop();
-                //   scope.startButton = 'Start Work';
-                //   scope.breakButton = 'Take Break';
-                //   scope.STOP_WATCH.workTime = scope.STOP_WATCH.defaultWorkTime;
-                //   scope.STOP_WATCH.breakTime = scope.STOP_WATCH.defaultBreakTime;
-                // };
-
                 var breakCountdown = function () {
-                    if (scope.STOP_WATCH.breakTime > 0) {
-                        scope.STOP_WATCH.breakTime--;
-                    } else if (scope.STOP_WATCH.breakTime === 0) {
+                    if (scope.STOP_WATCH.totalBreakTime > 0) {
+                        scope.STOP_WATCH.totalBreakTime--;
+                    } else if (scope.STOP_WATCH.totalBreakTime === 0) {
                         scope.onBreak = false;
                         scope.stop();
                         scope.breakButton = 'Take Break';
-                        scope.STOP_WATCH.breakTime = scope.STOP_WATCH.defaultBreakTime;
+                        scope.STOP_WATCH.totalBreakTime = scope.STOP_WATCH.defaultBreakTime;
                     }
                 }
+
+
 
                 scope.stop = function () {
                     $interval.cancel(promise);
                 }
 
                 scope.startStopButton = function (startButton) {
-
-                    //clockStarts
                     scope.startButton = (startButton === 'Start Work') ? 'Stop' : 'Start Work';
                     if (scope.startButton === 'Stop') {
                         promise = $interval(workCountdown, 1000);
                     } else if (scope.startButton === 'Start Work') {
                         $window.alert("Your Working Time Has Reset")
                         scope.stop();
-                        scope.STOP_WATCH.workTime = scope.STOP_WATCH.defaultWorkTime;
+                        scope.STOP_WATCH.totalWorkTime = scope.STOP_WATCH.defaultWorkTime;
                     }
                 };
 
@@ -81,12 +68,20 @@
 
 
                 scope.takeBreakButton = function (breakButton) {
-                    //clockStarts
-                    scope.breakButton = (breakButton === 'Take Break') ? 'Break In Session' : 'Take Break';
-                    if (scope.breakButton === 'Break In Session') {
-                        promise = $interval(breakCountdown, 1000);
+                    if (scope.breakButton === 'Take Long Break') {
+                        scope.breakButton = (breakButton === 'Take Long Break') ? 'Break In Session' : 'Take Long Break';
+                        if (scope.breakButton === 'Break In Session') {
+                            promise = $interval(breakCountdown, 1000);
+                        } else if (scope.breakButton === 'Take Long Break') {
+                            $window.alert("Your Working Time Is To Important.  Please Wait")
+                        }
                     } else if (scope.breakButton === 'Take Break') {
-                        $window.alert("Your Working Time Is To Important.  Please Wait")
+                        scope.breakButton = (breakButton === 'Take Break') ? 'Break In Session' : 'Take Break';
+                        if (scope.breakButton === 'Break In Session') {
+                            promise = $interval(breakCountdown, 1000);
+                        } else if (scope.breakButton === 'Take Break') {
+                            $window.alert("Your Working Time Is To Important.  Please Wait")
+                        }
                     }
                 };
 
